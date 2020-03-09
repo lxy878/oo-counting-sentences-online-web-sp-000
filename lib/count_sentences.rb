f517160272c9e6e82af60abd963525fa9321836e
Xiaoyi Liu <lxy878@gmail.com>
2020-03-09 01:10:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    self.split.each {|word| count = (word.end_with?(".")||word.end_with?("!")||word.end_with?("?"))? count+1 : count}
+    count
   end
 end
