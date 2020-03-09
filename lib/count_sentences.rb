d2eb204a4f9631eee9a94c75220380fee6b00a7d
Xiaoyi Liu <lxy878@gmail.com>
2020-03-09 01:10:01 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,7 @@ class String
   end
 
   def count_sentences
-
+    count = 0
+    self.split.each {|word| count = (word.end_with?(".")||word.end_with?("!")||word.end_with?("?"))? count+1 : count}
   end
 end
